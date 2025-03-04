b5c472b6bdb9fb88d02aaff25ce0dbe819bf63e4
Misael <misamr95@gmail.com>
2025-03-03 18:15:10 -0600

    Actuliza el app

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function agregarAmigo() {
+    const entrada = document.getElementById("amigo");
+    const nombre = entrada.value.trim();
+
+    if (nombre === "")
+    {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+
+    listaDeAmigos.push(nombre);
+    actualizarLista();
+    entrada.value = "";
+}
+
+function actualizarLista() {
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    listaDeAmigos.forEach((amigo, index) => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (listaDeAmigos.length === 0)
+    {
+        alert("Por favor, agregue al menos un nombre para realizar el sorteo.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+    const amigoSorteado = listaDeAmigos[indiceAleatorio];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    const li = document.createElement("li");
+    li.textContent = `El amigo secreto es: ${amigoSorteado}`;
+    resultado.appendChild(li);
+}
